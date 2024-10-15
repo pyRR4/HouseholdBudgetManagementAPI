1d22c3d5f2206187da1c6e8856f8df9fa75a87df
Igor <igopood33@gmail.com>
2024-10-15 23:30:47 +0200

    Created UserEntity basics

---
+++ src/main/java/com/example/transactions/service/UserEntity.java
@@ -0,0 +1,31 @@
+package com.example.transactions.service;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long user_id;
+    private String username;
+    private String password;
+    private String nickname;
+    private String email;
+
+    public UserEntity() {}
+
+    public UserEntity(String username, String password, String nickname, String email) {
+        this.username = username;
+        this.password = password;
+        this.nickname = nickname;
+        this.email = email;
+    }
+
+}
