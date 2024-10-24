df6a0dc6543e41c072c3942e130fd0c3053427e4
Igor <igopood33@gmail.com>
2024-10-24 21:08:13 +0200

    Connected users with transactions, switched date to localdatetime and started on userservice

--- src/main/java/com/example/transactions/controller/TransactionResponse.java
+++ src/main/java/com/example/transactions/controller/TransactionResponse.java
@@ -5,8 +5,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
-import java.util.Date;
-
 @Getter
 @Setter
 public class TransactionResponse { //DTO class
--- src/main/java/com/example/transactions/service/TransactionEntity.java
+++ src/main/java/com/example/transactions/service/TransactionEntity.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.text.DateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
