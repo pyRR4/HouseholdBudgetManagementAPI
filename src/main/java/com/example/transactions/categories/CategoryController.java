b8f91d98d65d724551cd95bb725bebb0276cf33e
Igor <igopood33@gmail.com>
2024-10-28 16:34:05 +0100

    fixed errors (no bean annotations)

--- src/main/java/com/example/transactions/categories/CategoryController.java
+++ src/main/java/com/example/transactions/categories/CategoryController.java
@@ -1,5 +1,8 @@
 package com.example.transactions.categories;
 
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class CategoryController {
+
 }
--- src/main/java/com/example/transactions/categories/CategoryService.java
+++ src/main/java/com/example/transactions/categories/CategoryService.java
@@ -1,12 +1,16 @@
 package com.example.transactions.categories;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Service
 public class CategoryService {
 
     private final CategoryRepository categoryRepository;
 
+    @Autowired
     public CategoryService(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
     }
